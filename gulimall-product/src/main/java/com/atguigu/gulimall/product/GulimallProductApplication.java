fe12f0f1a3e14e0344210c8fcb78787c571cdc42
spongzi <lxlandsx@outlook.com>
2022-07-16 16:55:58 +0800

    项目初始化创建

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Spongzi
+ */
+@SpringBootApplication
+public class GulimallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProductApplication.class, args);
+    }
+
+}
